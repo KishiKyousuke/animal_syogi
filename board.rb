46d5d1104f51ade84b2f75fb0e43b4dcd4b96b57
KishiKyousuke <carp011808pg@icloud.com>
2024-01-13 21:51:41 +0900

    ボードのattribute名を変更

--- board.rb
+++ board.rb
@@ -1,5 +1,5 @@
 class Board
-  def initialize(animals)
+  def initialize(placed_animals:)
     @position = [
       {A: nil, B: nil, C: nil},
       {A: nil, B: nil, C: nil},
@@ -7,8 +7,10 @@ class Board
       {A: nil, B: nil, C: nil},
     ]
     # 盤上に存在している動物
-    @animals = animals
+    @placed_animals = placed_animals
   end
+
+  attr_accessor :position, :placed_animals
 
   def movable?(current_position, moving_position)
     # TODO: 指定された位置に移動可能かを判定する
