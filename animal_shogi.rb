3480838a2182a01fb27c1509e7d599494dd2f932
KishiKyousuke <carp011808pg@icloud.com>
2024-01-13 23:20:58 +0900

    実行ファイルの作成

---
+++ animal_shogi.rb
@@ -0,0 +1,13 @@
+require_relative 'animal'
+require_relative 'animal/lion'
+require_relative 'animal/elephant'
+require_relative 'animal/giraffe'
+require_relative 'animal/chick'
+require_relative 'animal/chicken'
+require_relative 'move_instruction_parser'
+require_relative 'player'
+require_relative 'board'
+require_relative 'game'
+
+game = Game.new
+game.start
