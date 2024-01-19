114f1672d0967c57020fa48dcb24b04ff1c7b663
KishiKyousuke <carp011808pg@icloud.com>
2024-01-20 00:18:30 +0900

    MoveInstructionParserのテストを追加

---
+++ spec/model/move_instruction_parser_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../../move_instruction_parser'
+require_relative '../../animal'
+require_relative '../../animal/elephant'
+require_relative '../../animal/lion'
+require_relative '../../animal/giraffe'
+require_relative '../../animal/chick'
+
+RSpec.describe MoveInstructionParser do
+  describe '#parse' do
+    it '与えられた指示を解析してhashを返すこと' do
+      # 先手の初期配置の指定
+      expect(MoveInstructionParser.new('A4E').parse).to eq({ column_key: :A, row_index: 3, animal_class: Animal::Elephant })
+      expect(MoveInstructionParser.new('B4L').parse).to eq({ column_key: :B, row_index: 3, animal_class: Animal::Lion })
+      expect(MoveInstructionParser.new('C4G').parse).to eq({ column_key: :C, row_index: 3, animal_class: Animal::Giraffe })
+      expect(MoveInstructionParser.new('B3C').parse).to eq({ column_key: :B, row_index: 2, animal_class: Animal::Chick })
+
+      # 後手の初期配置の指定
+      expect(MoveInstructionParser.new('A1G').parse).to eq({ column_key: :A, row_index: 0, animal_class: Animal::Giraffe })
+      expect(MoveInstructionParser.new('B1L').parse).to eq({ column_key: :B, row_index: 0, animal_class: Animal::Lion })
+      expect(MoveInstructionParser.new('C1E').parse).to eq({ column_key: :C, row_index: 0, animal_class: Animal::Elephant })
+      expect(MoveInstructionParser.new('B2C').parse).to eq({ column_key: :B, row_index: 1, animal_class: Animal::Chick })
+    end
+  end
+end
