49649618694aa7a827318a137eff8e303836fb59
KishiKyousuke <carp011808pg@icloud.com>
2024-01-20 11:51:16 +0900

    playerのspecを作成

---
+++ spec/model/player_spec.rb
@@ -0,0 +1,30 @@
+RSpec.describe Player do
+  describe '#first_move?' do
+    let!(:player) { Player.new(animals_in_hand: [], first_move: first_move) }
+
+    context '先手の場合' do
+      let(:first_move) { true }
+
+      it { expect(player.first_move?).to eq true }
+    end
+
+    context '後手の場合' do
+      let(:first_move) { false }
+
+      it { expect(player.first_move?).to eq false }
+    end
+  end
+
+  describe '#show_hands' do
+    let!(:player) { Player.new(animals_in_hand: [], first_move: false) }
+    let(:animals_in_hand) { [Animal::Chick.new(possession_player: player), Animal::Giraffe.new(possession_player: player)] }
+
+    before do
+      player.instance_variable_set(:@animals_in_hand, animals_in_hand)
+    end
+
+    it '手持ちの動物の省略名の文字列を返すこと' do
+      expect(player.show_hands).to eq 'c, g'
+    end
+  end
+end
